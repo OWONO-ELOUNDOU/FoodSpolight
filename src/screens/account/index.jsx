04b42f84e8d064169a5fdfd5f56341a2ea77d448
OWONO ELOUNDOU <OWONO ELOUNDOU>
2023-01-04 17:33:47 +0100

    Ajout de nouvelles pages

---
+++ src/screens/account/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+const accountScreen = () => {
+    return(
+        <View>
+            <Text>
+                Account Screen
+            </Text>
+        </View>
+    )
+}
+
+export default accountScreen;
---
+++ src/screens/home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+const HomeScreen = () => {
+    return(
+        <View style={ styles.container } >
+            <Text>
+                Home Screen
+            </Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
